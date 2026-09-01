7ae64faf6a751ddf6ab13134f15702d91ce1e7c3
sysadmin ohana <alohapublicinform@gmail.com>
2026-09-01 20:09:32 +0000

    🎉 Manu's Top Party Picks + live highlight reel (mirror to domain deployment)

---
+++ data/party-picks.js
@@ -0,0 +1,499 @@
+// 🎉 MANU'S TOP PARTY PICKS — BM 2026
+// Source: party flyers collected by Manu · converted from burning_man_2026_pretty_schedule.csv
+// Edit + bump updated; never rename const PARTY or its keys.
+const PARTY = {
+ "updated": "2026-09-01 13:46 PT",
+ "credit": "Curated by Manu Alzuru 🎉 — Open Source Orchestra Node 004, riding with the turtles · from party flyers posted to camp",
+ "days": [
+  "Monday · Aug 31",
+  "Tuesday · Sep 1",
+  "Wednesday · Sep 2",
+  "Thursday · Sep 3",
+  "Friday · Sep 4",
+  "Saturday · Sep 5",
+  "Sunday · Sep 6",
+  "Monday · Sep 7"
+ ],
+ "picks": [
+  {
+   "day": "Monday · Aug 31",
+   "time": "00:00-03:30",
+   "artist": "Chafic b2b John Woods; Gallivanter",
+   "camp": "Sahar Camp",
+   "where": "9:15 & J",
+   "by": "@brunomregueira"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "05:00-07:00",
+   "artist": "Bipolar Express",
+   "camp": "The Singularity Tree",
+   "where": "Deep Playa",
+   "by": "@martenlou"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "15:00-00:30",
+   "artist": "Kimono Disco",
+   "camp": "Miki Beach",
+   "where": "10 & F",
+   "by": "@mikibeachcamp"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "19:00-19:45",
+   "artist": "RJ with LP Giobbi",
+   "camp": "Above the Noise",
+   "where": "",
+   "by": "RJ"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "19:30-20:30",
+   "artist": "Sahar Opening",
+   "camp": "Sahar Camp",
+   "where": "9:15 & J",
+   "by": "@brunomregueira"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "from 20:00; individual slots not shown",
+   "artist": "Hot Stuff lineup: Edge; Vermilion b2b Miss Behavior; Alvaro; Justin Shaffer; Topper",
+   "camp": "Hot Stuff",
+   "where": "2 & F",
+   "by": "@camphotstuff"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "20:00-02:00",
+   "artist": "Hugo A; Mona Pirzad; Igor Marijuan; Kotoe",
+   "camp": "Camp Albortz",
+   "where": "E & 9:45",
+   "by": "Camp Albortz"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "21:00-22:00 opening fire ceremony",
+   "artist": "La Huaira",
+   "camp": "Sahar Camp",
+   "where": "9:15 & J",
+   "by": "@brunomregueira"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "22:00-23:30",
+   "artist": "Julia Sandstorm",
+   "camp": "Playground",
+   "where": "2 & C",
+   "by": "@juliasandstorm"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "22:15-00:00 live",
+   "artist": "Kel Assouf + Santiago Bartolome & Friends",
+   "camp": "Sahar Camp",
+   "where": "9:15 & J",
+   "by": "@brunomregueira"
+  },
+  {
+   "day": "Monday · Aug 31",
+   "time": "23:00-00:30",
+   "artist": "RJ",
+   "camp": "Miki Beach",
+   "where": "10 & F",
+   "by": "RJ"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "01:00-02:00 (Tuesday/Wednesday source ambiguity)",
+   "artist": "Julia Sandstorm; LP Giobbi; Hedda Stenberg; Riche",
+   "camp": "B4B Takeover",
+   "where": "",
+   "by": "@juliasandstorm"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "01:00-03:00",
+   "artist": "Magisus & Friends",
+   "camp": "Playalchemist",
+   "where": "3:30 & E",
+   "by": "@magisus.mathi"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "02:00-04:00 private DM",
+   "artist": "Leon Hong Kong",
+   "camp": "Day Dream",
+   "where": "2:15 & B",
+   "by": "@leon_nds"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "02:30-04:00",
+   "artist": "Anna",
+   "camp": "Playground",
+   "where": "Arrival Stage",
+   "by": "@djannaofficial"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "06:00-07:30",
+   "artist": "Roy Rosenfeld",
+   "camp": "Long Feng",
+   "where": "10 & K",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "11:00-sunset Friday event; roster reference",
+   "artist": "Anna; Deer Jade; Jamiie; Kotoe; LaserLaura; Maxi Meraki; Philou; Sam Shure",
+   "camp": "I AM YOU YOU ARE ME / Sahar",
+   "where": "9:15 & J",
+   "by": "@this_is_jamiie"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "12:00-14:00",
+   "artist": "Eli Light & Lau",
+   "camp": "AMMA",
+   "where": "2 & Iroko",
+   "by": "AMMA"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "14:00-16:00",
+   "artist": "Isabeau Fort b2b Golden",
+   "camp": "AMMA",
+   "where": "2 & Iroko",
+   "by": "AMMA"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "16:00-18:00",
+   "artist": "Madmotor Miquel b2b Kotoe",
+   "camp": "AMMA",
+   "where": "2 & Iroko",
+   "by": "AMMA"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "18:00-03:30",
+   "artist": "Ameme; Anna; Ines Rau; Jamiie; Jojo Abot; Pego; Rockin Moroccin; Tom & Collins; Yung Omz",
+   "camp": "One Vibe One Tribe / Sahar",
+   "where": "9:15 & J",
+   "by": "@rockinmoroccin"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "18:00-20:00",
+   "artist": "Roy Rosenfeld",
+   "camp": "AMMA",
+   "where": "2 & I",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "18:00-20:00",
+   "artist": "Roy Rosenfeld",
+   "camp": "AMMA",
+   "where": "2 & Iroko",
+   "by": "AMMA"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "19:30",
+   "artist": "Oliver Koletzki & Frida Darko",
+   "camp": "Treblemaker",
+   "where": "2:30 & Esplanade",
+   "by": "@oliverkoletzki"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "20:00-22:00",
+   "artist": "Jamyn Phoenix; Marvin Jam & Sese",
+   "camp": "AMMA",
+   "where": "2 & Iroko",
+   "by": "AMMA"
+  },
+  {
+   "day": "Tuesday · Sep 1",
+   "time": "from 22:00",
+   "artist": "Hedda Stenberg; Mia Twin; Julia Sandstorm; Philou",
+   "camp": "Norse Code",
+   "where": "8:45 & Eternal",
+   "by": "Norse Code"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "00:30-03:30",
+   "artist": "Ameme b2b Vanjee",
+   "camp": "Sahar x One Tribe",
+   "where": "9:15 & J",
+   "by": "@amemedj"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "00:30-02:30",
+   "artist": "Roy Rosenfeld b2b Vanjee",
+   "camp": "Nova",
+   "where": "Deep Playa",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "01:30",
+   "artist": "Oliver Koletzki & Frida Darko",
+   "camp": "Eden Art Car",
+   "where": "",
+   "by": "@oliverkoletzki"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "13:30-22:30",
+   "artist": "Pretty in Pink",
+   "camp": "Miki Beach",
+   "where": "10 & F",
+   "by": "@mikibeachcamp"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "17:00-19:00",
+   "artist": "Roy Rosenfeld b2b Rafael",
+   "camp": "Discotique",
+   "where": "2 & G",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "18:00-08:00",
+   "artist": "Tommy Raffa; Xinobi b2b Losha; Madmotor Miquel b2b Kotoe; Nacho Isa b2b Manguito & Ksenyeah; Maxxe b2b Von Davis; Karma; Love Doctor",
+   "camp": "Animal Party / Go Down Under",
+   "where": "10 & E",
+   "by": "@maxxe.music"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "19:00-21:00",
+   "artist": "RJ b2b Arodes",
+   "camp": "Pink Mammoth",
+   "where": "9 & G Plaza",
+   "by": "RJ"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "20:00-02:00",
+   "artist": "Oliver Koletzki b2b Frida Darko; Roman Kyn; Lovecraft",
+   "camp": "Camp Albortz",
+   "where": "E & 9:45",
+   "by": "Camp Albortz"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "23:30-01:30",
+   "artist": "Magisus b2b Emilio + Manik Khan live",
+   "camp": "Bisous Tea House",
+   "where": "8:45 & E",
+   "by": "@magisus.mathi"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "concert listing",
+   "artist": "Jojo Abot presents Love Poceans",
+   "camp": "AMMA",
+   "where": "",
+   "by": "enteramma"
+  },
+  {
+   "day": "Wednesday · Sep 2",
+   "time": "",
+   "artist": "Nico Stojan remembrance + Marques Wyatt",
+   "camp": "AMMA / Temple / Bisous",
+   "where": "",
+   "by": "img_92e64b9dd056.jpg"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "until 02:00",
+   "artist": "Oliver Koletzki b2b Frida Darko (program continues)",
+   "camp": "Camp Albortz",
+   "where": "E & 9:45",
+   "by": "Camp Albortz"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "02:00-03:30",
+   "artist": "Rodriguez Jr. & Friends",
+   "camp": "Twilight Language",
+   "where": "10 & I",
+   "by": "RJ"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "06:00-09:30",
+   "artist": "Roy Rosenfeld",
+   "camp": "Tabernacle",
+   "where": "10 & E",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "07:00-09:00",
+   "artist": "Long Feng b2b Darco",
+   "camp": "Long Feng",
+   "where": "10 & K",
+   "by": "@rafael___music"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "14:00",
+   "artist": "Tom & Collins",
+   "camp": "Discotique",
+   "where": "2 & G",
+   "by": "@tomandcollins"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "17:00-19:00",
+   "artist": "Roy Rosenfeld",
+   "camp": "Pink Mammoth",
+   "where": "9 & G",
+   "by": "Roy Rosenfeld"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "18:00-18:45 meetup/start",
+   "artist": "Solaris b2b Onthefritz; SYOST; Eulen Aus Chrom; Foda; Odeon; Lynn; Nexus",
+   "camp": "Disorient Express Art Car",
+   "where": "meet 9:15 & J",
+   "by": "@lucasereth"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "18:30-21:00",
+   "artist": "Ameme experimental set",
+   "camp": "Bunny Meth Lab",
+   "where": "9:30 & Esplanade",
+   "by": "@amemedj"
+  },
+  {
+   "day": "Thursday · Sep 3",
+   "time": "19:00 onward",
+   "artist": "Catori; Haana; Aiwaa; Eli Light; Zenti",
+   "camp": "Caravan D'Light",
+   "where": "8:15 & Eternal",
+   "by": "Caravan D'Light"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "07:30-09:00",
+   "artist": "Ameme b2b Vanjee",
+   "camp": "Long Feng",
+   "where": "10 & K",
+   "by": "@amemedj"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "11:00-sunset",
+   "artist": "Jamiie; Anna; Deer Jade; Kotoe; LaserLaura; Maxi Meraki; Philou; Sam Shure",
+   "camp": "I AM YOU YOU ARE ME / Sahar",
+   "where": "9:15 & J",
+   "by": "@this_is_jamiie"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "13:30-22:30",
+   "artist": "Beach Bash",
+   "camp": "Miki Beach",
+   "where": "10 & F",
+   "by": "@mikibeachcamp"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "14:00-16:00",
+   "artist": "Magisus",
+   "camp": "AMMA",
+   "where": "2 & I",
+   "by": "@magisus.mathi"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "16:30-18:00",
+   "artist": "Rampue b2b Adaamar",
+   "camp": "Miki Beach",
+   "where": "10 & F",
+   "by": "@rampue"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "17:00-18:00",
+   "artist": "Anna",
+   "camp": "Sahar Camp",
+   "where": "",
+   "by": "@djannaofficial"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "18:30-20:00",
+   "artist": "Rafael",
+   "camp": "Opulent Temple",
+   "where": "10 & Esplanade",
+   "by": "@rafael___music"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "from 20:00",
+   "artist": "Hot Stuff lineup: Will Kayll; Manfredi b2b Vitz; Aline Brooklyn b4b David Triana b4b Clovis b4b Topper; Shaun Reeves b2b Aline Brooklyn",
+   "camp": "Hot Stuff",
+   "where": "2 & F",
+   "by": "@camphotstuff"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "21:00-23:00",
+   "artist": "Anna",
+   "camp": "Long Feng / Huo Feng",
+   "where": "",
+   "by": "@djannaofficial"
+  },
+  {
+   "day": "Friday · Sep 4",
+   "time": "21:00-23:00",
+   "artist": "Anna b2b Jackson",
+   "camp": "Favela Art Car",
+   "where": "",
+   "by": "@djannaofficial"
+  },
+  {
+   "day": "Saturday · Sep 5",
+   "time": "04:00 after Man Burn",
+   "artist": "Long Feng b2b Xinobi",
+   "camp": "Long Feng",
+   "where": "10 & K",
+   "by": "@foreverannicka"
+  },
+  {
+   "day": "Saturday · Sep 5",
+   "time": "time not shown",
+   "artist": "Ameme; Santiago Garcia; Hedda Stenberg; Slim Soul; Marques Wyatt; Todd",
+   "camp": "Norse Code Man Burn After Party",
+   "where": "8:45 & Eternal",
+   "by": "Norse Code"
+  },
+  {
+   "day": "Sunday · Sep 6",
+   "time": "23:00-late",
+   "artist": "Sahar Moment to Integrate: Dillon Porter; Jac Hoja; Katya Lee; Kel Assouf; Mehry Eslaminia; Neon Dots; Santiago Bartolome; Unders",
+   "camp": "Sahar Camp",
+   "where": "9:15 & J",
+   "by": "Sahar Camp"
+  },
+  {
+   "day": "Monday · Sep 7",
+   "time": "19:00-late; runs Sep 7-9",
+   "artist": "Marques Wyatt; Oona Dahl; Lee Reynolds; Nikita & Greg Eversoul; Bilaliwood; Maria Tambien; Umee Dee; Yaku; Side Quest w Shaun Reeves",
+   "camp": "Reality Check Reno",
+   "where": "Reno NV",
+   "by": "@marqueswyatt"
+  }
+ ]
+};
